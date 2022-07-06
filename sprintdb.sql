442fef7e4eb74e7b1a594ecce5b35b7d7a726794
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-07-06 22:28:48 +0300

    updated database file

--- sprintdb.sql
+++ sprintdb.sql
@@ -23,11 +23,14 @@ DROP TABLE IF EXISTS `employees`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employees` (
-  `EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
-  `EmployeeName` varchar(255) NOT NULL,
-  `ProjectID` int(11) NOT NULL,
-  PRIMARY KEY (`EmployeeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `addedProject` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `addedProject` (`addedProject`),
+  CONSTRAINT `employess` FOREIGN KEY (`addedProject`) REFERENCES `projects` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +39,7 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (1,'Jonny',2),(2,'Robert',2),(3,'Sara',0),(4,'Sofija',3),(5,'Margaret',1),(6,'Tim',1);
+INSERT INTO `employees` VALUES (1,'Jonny','Dep',1),(2,'Margaret','Stiuart',2),(3,'John','McKenna',2),(4,'Barbara','Madison',5),(5,'Sara','Cromwell',3),(6,'Tim','Hope',4),(7,'Paul','West',4),(8,'Sofija','Gonzales',5),(9,'Phill','Wilson',1);
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,10 +51,11 @@ DROP TABLE IF EXISTS `projects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `projects` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ProjectName` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ProjectName` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ProjectName` (`ProjectName`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +64,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'Java'),(2,'C#'),(3,'Laravel');
+INSERT INTO `projects` VALUES (3,'JAVA'),(5,'JAVASCRIPT'),(2,'LARAVEL'),(1,'PHP'),(4,'REACT');
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -73,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-04 21:53:06
+-- Dump completed on 2022-07-06 22:24:22
